800fee5e1a3142d2c4828bf69b42a65d77be37e6
vivancomiguel <113225991+vivancomiguel@users.noreply.github.com>
2022-11-08 01:51:07 -0600

    feat: Archivo LlenarImagenes

---
+++ LlenarImagenes.sql
@@ -0,0 +1,13 @@
+-- En desarrollo, faltan de subir imagenes
+
+-- Insert de las imagenes de ositos
+INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
+	('https://ibb.co/Q86N3hX', 1),
+  ('https://ibb.co/72xj2BT', 1),
+  ('https://ibb.co/jzTH3X1', 1),
+  ('https://ibb.co/zJjQGC9', 2),
+  ('https://ibb.co/xG64x6B', 51);
+
+-- Insert de las imagenes de adicionales
+INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
+  ('https://ibb.co/mhHt0Nj', 64);
