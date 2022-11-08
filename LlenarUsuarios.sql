331fad286836ad3e1fba1a415dfad7fa30da8807
Clarisa Betancourt <lcbeg@ier.unam.mx>
2022-11-07 23:04:07 -0600

    Feat: script para llenar tabla con informacion de usuarios

--- LlenarUsuarios.sql
+++ LlenarUsuarios.sql
@@ -1,16 +1,17 @@
 USE mydb;
 
 INSERT INTO `usuarios` ( `correo`,`contraseña`, `nombre_c`, `telefono`)
-VALUES ('betancourt.lucero@gmail.com', 'ThorMicaela22', 'Clarisa Betancourt', '5539192506');
+VALUES ('moran.miguel@gmail.com', 'TaylorSwift99', 'Miguel Moran', '3335453839');
 INSERT INTO `usuarios` ( `correo`,`contraseña`, `nombre_c`, `telefono`)
-VALUES ('.lucero@gmail.com', 'ThorMicaela22', 'Clarisa Betancourt', '5539192506');
+VALUES ('vivanco.miguel@gmail.com', '4321añesartnoc', 'Miguel Vivanco', '3332567522');
 INSERT INTO `usuarios` ( `correo`,`contraseña`, `nombre_c`, `telefono`)
-VALUES ('Gerardo.Audrey@gmail.com', 'Luka2017', 'Audrey Gerardo Juarez', '22245634');
+VALUES ('Abelardo97@gmail.com', 'contraseña1234', 'Abelardo Gutierrez', '6332435677');
+INSERT INTO `usuarios` ( `correo`,`contraseña`, `nombre_c`, `telefono`)
+VALUES ('Gerardo.Audrey@gmail.com', 'Luka2017', 'Audrey Gerardo Juarez', '2224563499');
 INSERT INTO `usuarios` ( `correo`,`contraseña`, `nombre_c`, `telefono`)
 VALUES ('rdz.fernanda@gmail.com', 'Hilario2020', 'Fernanda Rodríguez Palapa', '5522356532');
 INSERT INTO `usuarios` ( `correo`,`contraseña`, `nombre_c`, `telefono`)
 VALUES ('betancourt.lucero@gmail.com', 'ThorMicaela22', 'Clarisa Betancourt García', '5539390506');
 
-SELECT * FROM usuarios;
 
 
