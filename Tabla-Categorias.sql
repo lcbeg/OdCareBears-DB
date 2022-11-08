7fd82edcd0536aaef05d61eba36761a6dce37c82
Fernanda Palapa <fernandabqd@gmail.com>
2022-11-07 23:05:47 -0600

    tabla categorias

---
+++ Tabla-Categorias.sql
@@ -0,0 +1,7 @@
+USE `mydb`;
+SELECT * FROM `mydb`.`categorias`;
+INSERT INTO `mydb`.`categorias`(nombre) VALUE ('Ositos');
+INSERT INTO `mydb`.`categorias`(nombre) VALUE ('Accesorios');
+INSERT INTO `mydb`.`categorias`(nombre) VALUE ('Ceramica');
+INSERT INTO `mydb`.`categorias`(nombre) VALUE ('Otros');
+SELECT * FROM `mydb`.`categorias`;
