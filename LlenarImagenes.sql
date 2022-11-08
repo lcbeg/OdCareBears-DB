870f61daba73501a114c2fa94cbaed45c2b78056
vivancomiguel <113225991+vivancomiguel@users.noreply.github.com>
2022-11-08 02:05:39 -0600

    feat: Imagenes ositos

--- LlenarImagenes.sql
+++ LlenarImagenes.sql
@@ -1,12 +1,24 @@
 -- En desarrollo, faltan de subir imagenes
 
 -- Insert de las imagenes de ositos
+-- Insert de las imagenes de ositos
 INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
 	('https://ibb.co/Q86N3hX', 1),
-  ('https://ibb.co/72xj2BT', 1),
-  ('https://ibb.co/jzTH3X1', 1),
-  ('https://ibb.co/zJjQGC9', 2),
-  ('https://ibb.co/xG64x6B', 51);
+    ('https://ibb.co/72xj2BT', 1),
+    ('https://ibb.co/jzTH3X1', 1),
+    ('https://ibb.co/zJjQGC9', 2),
+    ('https://ibb.co/nP09Pfx', 2),
+    ('https://ibb.co/n7wBhxn', 2),
+    ('https://ibb.co/9tty0Tb', 3),
+    ('https://ibb.co/KrfYkgZ', 3),
+    ('https://ibb.co/9GGdx4f', 3),
+    ('https://ibb.co/vcs93xD', 4),
+    ('https://ibb.co/JKwKbs1', 4),
+    ('https://ibb.co/tcssfw4', 4),
+    ('https://ibb.co/t3pVbPz', 5),
+    ('https://ibb.co/WK5hdQ6', 5),
+    ('https://ibb.co/qpXp5sK', 5),
+    ('https://ibb.co/xG64x6B', 51);
 
 -- Insert de las imagenes de adicionales
 INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
