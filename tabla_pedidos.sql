a2607bbe7f1c0bb786d624ecf80e889d36f1e715
Abelardo Gutierrez Lopez <gabelardo04@gmail.com>
2022-11-08 00:56:19 -0600

    feat: se sube tabla de pedidos

---
+++ tabla_pedidos.sql
@@ -0,0 +1,21 @@
+USE `mydb`;
+SELECT * FROM `mydb`.`pedidos`;
+INSERT INTO `pedidos` ( `num_productos`,`direccion_envio`, `estado`,`usuario_id_usuario`)
+VALUES (2, 'Calle del Conde de Peñalver 16 28006 Guadalajara', 'Activo',1);
+
+INSERT INTO `pedidos` ( `num_productos`,`direccion_envio`, `estado`,`usuario_id_usuario`)
+VALUES (1, 'Avenida Piedras Negras 10 71980 Monterrey', 'Activo',2);
+
+INSERT INTO `pedidos` ( `num_productos`,`direccion_envio`, `estado`,`usuario_id_usuario`)
+VALUES (2, 'Avenida Siempre Viva 25 90900 CDMX', 'Activo',3);
+
+INSERT INTO `pedidos` ( `num_productos`,`direccion_envio`, `estado`,`usuario_id_usuario`)
+VALUES (4, 'Rincon Alegre 14  20165 Veracruz', 'Activo',4);
+
+INSERT INTO `pedidos` ( `num_productos`,`direccion_envio`, `estado`,`usuario_id_usuario`)
+VALUES (10, 'Esperanza Verde 15 78956 Xalapa', 'Activo',5);
+
+INSERT INTO `pedidos` ( `num_productos`,`direccion_envio`, `estado`,`usuario_id_usuario`)
+VALUES (2, 'Avenida Roma 257 10230 Nayarit', 'Activo',6);
+
+
