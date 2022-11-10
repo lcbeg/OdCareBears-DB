0d21042e5c66ffb43de01e0fa6368732a5c0f61e
vivancomiguel <113225991+vivancomiguel@users.noreply.github.com>
2022-11-09 21:37:22 -0600

    feat: Todas las direcciones de las imagenes

--- LlenarImagenes.sql
+++ LlenarImagenes.sql
@@ -1,25 +1,64 @@
--- En desarrollo, faltan de subir imagenes
+-- Ya estan las imagenes en estas direcciones, solo falta direccionarlos al producto correcto
 
--- Insert de las imagenes de ositos
 -- Insert de las imagenes de ositos
 INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
-	('https://ibb.co/Q86N3hX', 1),
-    ('https://ibb.co/72xj2BT', 1),
-    ('https://ibb.co/jzTH3X1', 1),
-    ('https://ibb.co/zJjQGC9', 2),
-    ('https://ibb.co/nP09Pfx', 2),
-    ('https://ibb.co/n7wBhxn', 2),
-    ('https://ibb.co/9tty0Tb', 3),
-    ('https://ibb.co/KrfYkgZ', 3),
-    ('https://ibb.co/9GGdx4f', 3),
-    ('https://ibb.co/vcs93xD', 4),
-    ('https://ibb.co/JKwKbs1', 4),
-    ('https://ibb.co/tcssfw4', 4),
-    ('https://ibb.co/t3pVbPz', 5),
-    ('https://ibb.co/WK5hdQ6', 5),
-    ('https://ibb.co/qpXp5sK', 5),
-    ('https://ibb.co/xG64x6B', 51);
-
+	('https://i.ibb.co/4sjTnCJ/Osito-Azul3.jpg', 1),
+    ('https://i.ibb.co/WgNGg7d/OsoAzul.jpg', 1),
+    ('https://i.ibb.co/G90FCSb/OsoAzul1.jpg', 1),
+    ('https://i.ibb.co/xHPXJw9/OsoCafe.jpg', 2),
+    ('https://i.ibb.co/vXBpXm0/OsoCafe1.jpg', 2),
+    ('https://i.ibb.co/wSd7D5w/OsoCafe2.jpg', 2),
+    ('https://i.ibb.co/744rDWn/Oso-Morado.jpg', 3),
+    ('https://i.ibb.co/VqzdZGy/Oso-Morado1.jpg', 3),
+    ('https://i.ibb.co/pnnkHKs/Oso-Morado2.jpg', 3),
+    ('https://i.ibb.co/dfbZP07/OsoRosa.jpg', 4),
+    ('https://i.ibb.co/Nsdskyv/OsoRosa1.jpg', 4),
+    ('https://i.ibb.co/7VWWcxX/OsoRosa2.jpg', 4),
+    ('https://i.ibb.co/mS5fHF0/Oso-Rosita.jpg', 5),
+    ('https://i.ibb.co/nkmGVTb/Oso-Rosita1.jpg', 5),
+    ('https://i.ibb.co/DQsQpCh/Oso-Rosita2.jpg', 5),
+    ('https://i.ibb.co/BjB10BQ/Osos.jpg', 51);
+-- Insert de las imagenes de accesorios
+INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
+	('https://i.ibb.co/6DpzTh0/20221103-104824.jpg', 6),
+    ('https://i.ibb.co/XW6mqdG/Aretes-Circulares2.jpg', 6),
+    ('https://i.ibb.co/4SmfSdW/Aretes-Circulares3.jpg', 6),
+    ('https://i.ibb.co/MP2Kyp0/Aretes-Circulares4.jpg', 7),
+    ('https://i.ibb.co/7y9fvZr/Aretes-Luna-Planeta.jpg', 8),
+    ('https://i.ibb.co/929ZSh8/Aretes-Luna-Planeta2.jpg', 8),
+    ('https://i.ibb.co/gJYMXY8/Aretes-Luna-Planeta3.jpg', 8),
+    ('https://i.ibb.co/84h6tGt/Estrella-Planeta.jpg', 7),
+    ('https://i.ibb.co/StmXdPB/Estrellitas.jpg', 9),
+    ('https://i.ibb.co/4RMFCJW/Luna-Azul1.jpg', 10),
+    ('https://i.ibb.co/3SKdMCf/Luna-Azul2.jpg', 10),
+    ('https://i.ibb.co/MfVR9R9/Luna-Estrellita.jpg', 11),
+    ('https://i.ibb.co/Zfb5cqr/Luna-Estrellita1.jpg', 11),
+    ('https://i.ibb.co/HBQVdtw/Luna-Estrellita2.jpg', 11),
+    ('https://i.ibb.co/JxHQzk1/Lunas.jpg', 12),
+    ('https://i.ibb.co/p0qHPbv/Noche-Estrellada1.jpg', 13),
+    ('https://i.ibb.co/1TbKn7s/Noche-Estrellada2.jpg', 13),
+    ('https://i.ibb.co/6FX24rj/Planeta-Aro.jpg', 14),
+    ('https://i.ibb.co/kJBSBkr/Planeta-Peque-o.jpg', 15);
+-- Insert de las imagenes de ceramica
+INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
+	('https://i.ibb.co/8KxkNZ6/Alajero.jpg', 16),
+    ('https://i.ibb.co/L8nh9mT/Alajero1.jpg', 16),
+    ('https://i.ibb.co/S3Ytyjf/Alajero2.jpg', 16),
+    ('https://i.ibb.co/3Njn98y/Cajita-Pastel.jpg', 17),
+    ('https://i.ibb.co/SdX9X2S/Cajita-Pastel1.jpg', 17),
+    ('https://i.ibb.co/smv2Nj7/Cajita-Pastel2.jpg', 17),
+    ('https://i.ibb.co/Vx2G6v2/Cajita-Pastel3.jpg', 17),
+    ('https://i.ibb.co/37pQK3V/Plato-Largo.jpg', 18),
+    ('https://i.ibb.co/k9nphkv/Plato-Largo2.jpg', 18),
+    ('https://i.ibb.co/wwk0Bmy/Taza.jpg', 19),
+    ('https://i.ibb.co/R0RZq44/Taza1.jpg', 19),
+    ('https://i.ibb.co/3BZVnY4/Taza2.jpg', 19),
+    ('https://i.ibb.co/FXS5QH3/tazafil.jpg', 52);
+-- Insert de las imagenes de otros
+INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
+	('https://i.ibb.co/wYPK6Tn/Pintura1.jpg', 20),
+    ('https://i.ibb.co/5WDfW7j/Pintura2.jpg', 20);
 -- Insert de las imagenes de adicionales
 INSERT INTO `imagen` (`enlace`,`productos_id_producto`) VALUES
-  ('https://ibb.co/mhHt0Nj', 64);
+	('https://i.ibb.co/6vLTc5F/accesoriofil.jpg', 53),
+    ('https://i.ibb.co/TLND302/otrosfil.jpg', 54);
